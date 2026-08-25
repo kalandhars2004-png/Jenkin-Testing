fe0bfe61016233906548c3a1705db28c199adcd2
kalandhar.s <kalandhars2004@gmail.com>
2026-08-25 16:37:56 +0530

    Update deployment destination

--- deploy.bat
+++ deploy.bat
@@ -4,8 +4,8 @@ echo ==========================================
 echo       HTML DEPLOYMENT STARTED
 echo ==========================================
 
-set SOURCE=%WORKSPACE%
-set DESTINATION=D:\Jenkins-Deployment\Jenkin-Testing
+set "SOURCE=%WORKSPACE%"
+set "DESTINATION=D:\MONTH-2\Week-4\tues\deployed"
 
 echo Source:
 echo %SOURCE%
@@ -13,16 +13,19 @@ echo %SOURCE%
 echo Destination:
 echo %DESTINATION%
 
-if not exist "%DESTINATION%" (
-    mkdir "%DESTINATION%"
+if exist "%DESTINATION%" (
+    echo Removing old deployment...
+    rmdir /S /Q "%DESTINATION%"
 )
 
-echo Copying files...
+mkdir "%DESTINATION%"
+
+echo Copying latest code...
 
 xcopy "%SOURCE%\*" "%DESTINATION%\" /E /I /Y /EXCLUDE:%SOURCE%\deploy-exclude.txt
 
 echo ==========================================
-echo       HTML DEPLOYMENT COMPLETED
+echo       DEPLOYMENT COMPLETED
 echo ==========================================
 
 exit /b 0
