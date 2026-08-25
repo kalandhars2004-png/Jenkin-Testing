832f7ccd0781bef81fa25d5a94d75849cbea6654
kalandhar.s <kalandhars2004@gmail.com>
2026-08-25 16:21:22 +0530

    Fix Jenkins deployment script

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,28 @@
-git add .
-git commit -m "Add Jenkins pipeline"
-git push
+@echo off
+
+echo ==========================================
+echo       HTML DEPLOYMENT STARTED
+echo ==========================================
+
+set SOURCE=%WORKSPACE%
+set DESTINATION=D:\Jenkins-Deployment\Jenkin-Testing
+
+echo Source:
+echo %SOURCE%
+
+echo Destination:
+echo %DESTINATION%
+
+if not exist "%DESTINATION%" (
+    mkdir "%DESTINATION%"
+)
+
+echo Copying files...
+
+xcopy "%SOURCE%\*" "%DESTINATION%\" /E /I /Y /EXCLUDE:%SOURCE%\deploy-exclude.txt
+
+echo ==========================================
+echo       HTML DEPLOYMENT COMPLETED
+echo ==========================================
+
+exit /b 0
